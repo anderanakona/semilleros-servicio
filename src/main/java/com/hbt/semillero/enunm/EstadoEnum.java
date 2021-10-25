34b883ea5ee558b7c0bac9cbd8de2b56766d9e0b
anderanakona <33234630+anderanakona@users.noreply.github.com>
2021-10-25 17:17:19 -0500

    COMIC ESTADO
    ES EL COMIT DEL DEL ESTADO

--- src/main/java/com/hbt/semillero/enunm/EstadoEnum.java
+++ src/main/java/com/hbt/semillero/enunm/EstadoEnum.java
@@ -1,5 +1,19 @@
 package com.hbt.semillero.enunm;
 
 public enum EstadoEnum {
+	VENDIDO("Vendido"),
+	COMPRADO("comprado");
+	
+	private String tipo;
+	
+
+	private EstadoEnum(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
 
 }
